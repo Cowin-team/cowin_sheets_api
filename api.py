02eaf9cb525814dd76c0ca8db4d331ee2bc582e3
Rakshith <rakshith.2905@gmail.com>
2021-05-05 00:53:02 -0700

    added wait timer

--- api.py
+++ api.py
@@ -4,15 +4,18 @@ from GSheets import GoogleSheets
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 sheets = GoogleSheets()
 
 @app.route('/update',methods = ['POST'])
+@cross_origin()
 def get_record():
-    record = json.loads(request.data)
-    sheets.update(record)
-    return jsonify(record)
+	if request.method == "POST": # The actual request following the preflight
+		print("\n\n",request.data,"\n\n")
+		record = json.loads(request.data)
+		resp = sheets.update(record)
+		return jsonify(resp)
 
 
 if __name__ == '__main__':
